c44449ac65f8ba888f654d90e1affe06fef8966b
igi iroko <alipede@igi-iroko.com>
2023-03-15 13:28:27 +0000

    fixed 404 navigation bug with = res.render("404", { path: "", pageTitle: "404 Not Found" });

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  // res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.render('404', { path: '', pageTitle: '404 Not Found' });
 });
 
 app.listen(3000);
